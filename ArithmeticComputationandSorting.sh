f870f4db5fb81d1d97acfaab51ac38ce2af994e8
NethajiChowdary <nethajichowdary555@gmail.com>
2021-06-04 13:53:21 +0530

    Read the values from Dictionary into Tte Array

--- ArithmeticComputationandSorting.sh
+++ ArithmeticComputationandSorting.sh
@@ -1,5 +1,6 @@
 echo "Welcome to Arithmetic Computation and Sorting"
 declare -A result
+declare -a resultarray
 read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
@@ -11,4 +12,10 @@ result[0]=$arithmeticOp1
 result[1]=$arithmeticOp2
 result[2]=$arithmeticOp3
 result[3]=$arithmeticOp4
-echo "result: ${result[@]}"
+
+for (( i=0; i<${#result[@]}; i++ ))
+do
+	resultarray[i]=${result[$i]}
+done
+
+echo "resultarray: ${resultarray[@]}"
